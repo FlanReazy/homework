c5ca2e9ceee4ee216c01fd65f91f1c9757dd5f5a
111fanyj <yfan@edgenesis.com>
2023-12-24 19:30:59 +0800

    adapter

--- src/main/java/com/example/fan/demo/DemoApplication.java
+++ src/main/java/com/example/fan/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.fan.demo.design_patterns.creational.abstract_factory.AbstractFactoryPatternDemo;
 import com.example.fan.demo.design_patterns.mvc.MVCDemo;
+import com.example.fan.demo.design_patterns.structural.adapter.AdapterPatternDemo;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -12,7 +13,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		// new MVCDemo().main(new String[] { "fanyuji" });
-		new AbstractFactoryPatternDemo().main(new String[] { "fanyuji" });
+
+		// new AbstractFactoryPatternDemo().main(new String[] { "fanyuji" });
+		new AdapterPatternDemo().main(new String[] { "fanyuji" });
 	}
 
 }
